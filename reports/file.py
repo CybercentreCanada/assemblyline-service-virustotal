ed6d30dc24c669997240cd5fcee7020814085b69
cccs-rs <ryan.samaroo@cyber.gc.ca>
2023-04-27 17:43:10 +0000

    Rename section title to 'Code Insight'

--- reports/file.py
+++ reports/file.py
@@ -59,7 +59,7 @@ def v3(doc, file_name, av_processor: AVResultsProcessor):
                 info_section.add_subsection(info.yara_section(v))
 
             elif 'crowdsourced_ai_results' in k:
-                   [ResultSection(title_text=f'Crowdsourced AI Analysis - {s["source"]}', body=s['analysis'],
+                   [ResultSection(title_text=f'Code Insight by {s["source"]}', body=s['analysis'],
                                   heuristic=Heuristic(1001), auto_collapse=True, parent=info_section) for s in v]
         if info_section.subsections:
             main_section.add_subsection(info_section)
