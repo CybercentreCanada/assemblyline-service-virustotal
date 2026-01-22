bdc92410cc8f4e329c415024c12100e40f7dce15
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2026-01-22 13:52:28 +0000

    Use an LRU cache to avoid repeated requests to Elasticsearch

--- virustotal/cache/elasticsearch/client.py
+++ virustotal/cache/elasticsearch/client.py
@@ -1,7 +1,9 @@
 """Client to interact with Elasticsearch for cached VirusTotal reports."""
 
 import re
+from functools import lru_cache
 from hashlib import sha256
+from os import environ
 from time import time
 from typing import Dict, List, Optional
 
@@ -11,7 +13,20 @@ from elasticsearch import Elasticsearch
 
 from virustotal.cache.client import CacheClient
 
+LRU_CACHE_SIZE = int(environ.get("LRU_CACHE_SIZE", "1024"))
 HASH_MATCHER = re.compile(SHA256_REGEX)
+
+
+# Add LRU caching to reduce the number of Elasticsearch queries for repeated requests
+@lru_cache(maxsize=LRU_CACHE_SIZE)
+def mget(es_client: Elasticsearch, docs: List[Dict], cache: str) -> List[Dict]:
+    """Perform an MGET search against Elasticsearch to find all the VirusTotal reports in the collection.
+
+    Returns:
+        A list of reports.
+
+    """
+    return es_client.mget(docs=docs)
 
 
 class ElasticClient(CacheClient):
@@ -35,29 +50,33 @@ class ElasticClient(CacheClient):
             A map of the feed type to a list of reports.
 
         """
-        docs_list = []
         id_map = {}
+        search_results = []
         for feed, data in collection.items():
             for d in data:
                 if not HASH_MATCHER.match(d):
                     # Generate the expected document ID
                     d = sha256(d.encode()).hexdigest()
                 id_map.setdefault(feed, []).append(d)
-                # Add a operation to check every index for the document by ID
-                docs_list.extend([{"_id": d, "_index": index} for index in self.indices[feed]])
 
-        search_results = []
-        if docs_list:
-            # Iterate over searches in batches to avoid HTTP 413 exceptions
-            batch_size = 1000
-            i = 0
-            while i * batch_size < len(docs_list):
-                search_results += [
-                    r
-                    for r in self.client.mget(docs=docs_list[i * batch_size : (i + 1) * batch_size])["docs"]
-                    if r.get("found")
-                ]
-                i += 1
+                # Prepare the list of documents to search for
+                docs_list = [{"_id": d, "_index": index} for index in self.indices[feed]]
+
+                # Iterate over the documents in batches to avoid overwhelming Elasticsearch
+                batch_size = 1000
+                i = 0
+                while i * batch_size < len(docs_list):
+                    # Perform the MGET search while using the cached version to reduce redundant searches
+                    search_results += [
+                        r
+                        for r in mget(
+                            self.client,
+                            docs=docs_list[i * batch_size : (i + 1) * batch_size],
+                            cache=self._cached_version,
+                        )["docs"]
+                        if r.get("found")
+                    ]
+                    i += 1
 
         # Sort results by the most recent analysis
         search_results = sorted(
